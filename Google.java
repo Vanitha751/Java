f97109d25341ad60c27d1fffb4358580f52a1265
Vanitha751 <xworkzvanitha@gmail.com>
2023-10-28 10:06:14 +0530

    2

---
+++ Google.java
@@ -0,0 +1,355 @@
+package com.xworkz.september.things;
+
+public class Google {
+	    private String searchQuery;
+	    private int searchResults;
+	    private boolean safeSearchEnabled;
+	    private String language;
+	    private boolean adsEnabled;
+	    private String location;
+	    private boolean personalizedResults;
+	    private boolean voiceSearchEnabled;
+	    private boolean newsNotificationsEnabled;
+	    private boolean weatherNotificationsEnabled;
+	    private String theme;
+	    private String timezone;
+	    private boolean saveSearchHistory;
+	    private boolean locationHistoryEnabled;
+	    private boolean twoFactorAuthentication;
+	    private String accountName;
+	    private String profilePictureUrl;
+	    private int unreadEmails;
+	    private boolean emailNotificationsEnabled;
+	    private String calendarEvents;
+	    private boolean calendarNotificationsEnabled;
+	    private int driveStorageUsed;
+	    private String recentFiles;
+	    private boolean driveNotificationsEnabled;
+	    private boolean shoppingNotificationsEnabled;
+	    private boolean financeNotificationsEnabled;
+	    private boolean sportsNotificationsEnabled;
+	    private boolean entertainmentNotificationsEnabled;
+	    private boolean healthNotificationsEnabled;
+	    private boolean travelNotificationsEnabled;
+	    private boolean stockNotificationsEnabled;
+	    private boolean mapHistoryEnabled;
+	    private String savedPlaces;
+	    private String recentSearches;
+	    private boolean mapNotificationsEnabled;
+	    private int YouTubeSubscribers;
+	    private int YouTubeVideosUploaded;
+	    private boolean YouTubeNotificationsEnabled;
+	    private int photosUploaded;
+	    private boolean photoNotificationsEnabled;
+	    private int contactsCount;
+	    private boolean contactSyncEnabled;
+	    private int bookmarksCount;
+	    private boolean bookmarksSyncEnabled;
+	    private int browserHistoryCount;
+	    private boolean browserHistorySyncEnabled;
+	    private String languagePreferences;
+	    private String currency;
+	    private boolean autoCompleteEnabled;
+	    private boolean darkModeEnabled;
+		public String getSearchQuery() {
+			return searchQuery;
+		}
+		public void setSearchQuery(String searchQuery) {
+			this.searchQuery = searchQuery;
+		}
+		public int getSearchResults() {
+			return searchResults;
+		}
+		public void setSearchResults(int searchResults) {
+			this.searchResults = searchResults;
+		}
+		public boolean isSafeSearchEnabled() {
+			return safeSearchEnabled;
+		}
+		public void setSafeSearchEnabled(boolean safeSearchEnabled) {
+			this.safeSearchEnabled = safeSearchEnabled;
+		}
+		public String getLanguage() {
+			return language;
+		}
+		public void setLanguage(String language) {
+			this.language = language;
+		}
+		public boolean isAdsEnabled() {
+			return adsEnabled;
+		}
+		public void setAdsEnabled(boolean adsEnabled) {
+			this.adsEnabled = adsEnabled;
+		}
+		public String getLocation() {
+			return location;
+		}
+		public void setLocation(String location) {
+			this.location = location;
+		}
+		public boolean isPersonalizedResults() {
+			return personalizedResults;
+		}
+		public void setPersonalizedResults(boolean personalizedResults) {
+			this.personalizedResults = personalizedResults;
+		}
+		public boolean isVoiceSearchEnabled() {
+			return voiceSearchEnabled;
+		}
+		public void setVoiceSearchEnabled(boolean voiceSearchEnabled) {
+			this.voiceSearchEnabled = voiceSearchEnabled;
+		}
+		public boolean isNewsNotificationsEnabled() {
+			return newsNotificationsEnabled;
+		}
+		public void setNewsNotificationsEnabled(boolean newsNotificationsEnabled) {
+			this.newsNotificationsEnabled = newsNotificationsEnabled;
+		}
+		public boolean isWeatherNotificationsEnabled() {
+			return weatherNotificationsEnabled;
+		}
+		public void setWeatherNotificationsEnabled(boolean weatherNotificationsEnabled) {
+			this.weatherNotificationsEnabled = weatherNotificationsEnabled;
+		}
+		public String getTheme() {
+			return theme;
+		}
+		public void setTheme(String theme) {
+			this.theme = theme;
+		}
+		public String getTimezone() {
+			return timezone;
+		}
+		public void setTimezone(String timezone) {
+			this.timezone = timezone;
+		}
+		public boolean isSaveSearchHistory() {
+			return saveSearchHistory;
+		}
+		public void setSaveSearchHistory(boolean saveSearchHistory) {
+			this.saveSearchHistory = saveSearchHistory;
+		}
+		public boolean isLocationHistoryEnabled() {
+			return locationHistoryEnabled;
+		}
+		public void setLocationHistoryEnabled(boolean locationHistoryEnabled) {
+			this.locationHistoryEnabled = locationHistoryEnabled;
+		}
+		public boolean isTwoFactorAuthentication() {
+			return twoFactorAuthentication;
+		}
+		public void setTwoFactorAuthentication(boolean twoFactorAuthentication) {
+			this.twoFactorAuthentication = twoFactorAuthentication;
+		}
+		public String getProfilePictureUrl() {
+			return profilePictureUrl;
+		}
+		public void setProfilePictureUrl(String profilePictureUrl) {
+			this.profilePictureUrl = profilePictureUrl;
+		}
+		public String getAccountName() {
+			return accountName;
+		}
+		public void setAccountName(String accountName) {
+			this.accountName = accountName;
+		}
+		public int getUnreadEmails() {
+			return unreadEmails;
+		}
+		public void setUnreadEmails(int unreadEmails) {
+			this.unreadEmails = unreadEmails;
+		}
+		public boolean isEmailNotificationsEnabled() {
+			return emailNotificationsEnabled;
+		}
+		public void setEmailNotificationsEnabled(boolean emailNotificationsEnabled) {
+			this.emailNotificationsEnabled = emailNotificationsEnabled;
+		}
+		public String getCalendarEvents() {
+			return calendarEvents;
+		}
+		public void setCalendarEvents(String calendarEvents) {
+			this.calendarEvents = calendarEvents;
+		}
+		public boolean isCalendarNotificationsEnabled() {
+			return calendarNotificationsEnabled;
+		}
+		public void setCalendarNotificationsEnabled(boolean calendarNotificationsEnabled) {
+			this.calendarNotificationsEnabled = calendarNotificationsEnabled;
+		}
+		public String getRecentFiles() {
+			return recentFiles;
+		}
+		public void setRecentFiles(String recentFiles) {
+			this.recentFiles = recentFiles;
+		}
+		public int getDriveStorageUsed() {
+			return driveStorageUsed;
+		}
+		public void setDriveStorageUsed(int driveStorageUsed) {
+			this.driveStorageUsed = driveStorageUsed;
+		}
+		public boolean isDriveNotificationsEnabled() {
+			return driveNotificationsEnabled;
+		}
+		public void setDriveNotificationsEnabled(boolean driveNotificationsEnabled) {
+			this.driveNotificationsEnabled = driveNotificationsEnabled;
+		}
+		public boolean isShoppingNotificationsEnabled() {
+			return shoppingNotificationsEnabled;
+		}
+		public void setShoppingNotificationsEnabled(boolean shoppingNotificationsEnabled) {
+			this.shoppingNotificationsEnabled = shoppingNotificationsEnabled;
+		}
+		public boolean isFinanceNotificationsEnabled() {
+			return financeNotificationsEnabled;
+		}
+		public void setFinanceNotificationsEnabled(boolean financeNotificationsEnabled) {
+			this.financeNotificationsEnabled = financeNotificationsEnabled;
+		}
+		public boolean isSportsNotificationsEnabled() {
+			return sportsNotificationsEnabled;
+		}
+		public void setSportsNotificationsEnabled(boolean sportsNotificationsEnabled) {
+			this.sportsNotificationsEnabled = sportsNotificationsEnabled;
+		}
+		public boolean isEntertainmentNotificationsEnabled() {
+			return entertainmentNotificationsEnabled;
+		}
+		public void setEntertainmentNotificationsEnabled(boolean entertainmentNotificationsEnabled) {
+			this.entertainmentNotificationsEnabled = entertainmentNotificationsEnabled;
+		}
+		public boolean isHealthNotificationsEnabled() {
+			return healthNotificationsEnabled;
+		}
+		public void setHealthNotificationsEnabled(boolean healthNotificationsEnabled) {
+			this.healthNotificationsEnabled = healthNotificationsEnabled;
+		}
+		public boolean isTravelNotificationsEnabled() {
+			return travelNotificationsEnabled;
+		}
+		public void setTravelNotificationsEnabled(boolean travelNotificationsEnabled) {
+			this.travelNotificationsEnabled = travelNotificationsEnabled;
+		}
+		public boolean isStockNotificationsEnabled() {
+			return stockNotificationsEnabled;
+		}
+		public void setStockNotificationsEnabled(boolean stockNotificationsEnabled) {
+			this.stockNotificationsEnabled = stockNotificationsEnabled;
+		}
+		public String getSavedPlaces() {
+			return savedPlaces;
+		}
+		public void setSavedPlaces(String savedPlaces) {
+			this.savedPlaces = savedPlaces;
+		}
+		public boolean isMapHistoryEnabled() {
+			return mapHistoryEnabled;
+		}
+		public void setMapHistoryEnabled(boolean mapHistoryEnabled) {
+			this.mapHistoryEnabled = mapHistoryEnabled;
+		}
+		public String getRecentSearches() {
+			return recentSearches;
+		}
+		public void setRecentSearches(String recentSearches) {
+			this.recentSearches = recentSearches;
+		}
+		public boolean isMapNotificationsEnabled() {
+			return mapNotificationsEnabled;
+		}
+		public void setMapNotificationsEnabled(boolean mapNotificationsEnabled) {
+			this.mapNotificationsEnabled = mapNotificationsEnabled;
+		}
+		public int getYouTubeSubscribers() {
+			return YouTubeSubscribers;
+		}
+		public void setYouTubeSubscribers(int youTubeSubscribers) {
+			YouTubeSubscribers = youTubeSubscribers;
+		}
+		public int getYouTubeVideosUploaded() {
+			return YouTubeVideosUploaded;
+		}
+		public void setYouTubeVideosUploaded(int youTubeVideosUploaded) {
+			YouTubeVideosUploaded = youTubeVideosUploaded;
+		}
+		public boolean isYouTubeNotificationsEnabled() {
+			return YouTubeNotificationsEnabled;
+		}
+		public void setYouTubeNotificationsEnabled(boolean youTubeNotificationsEnabled) {
+			YouTubeNotificationsEnabled = youTubeNotificationsEnabled;
+		}
+		public int getPhotosUploaded() {
+			return photosUploaded;
+		}
+		public void setPhotosUploaded(int photosUploaded) {
+			this.photosUploaded = photosUploaded;
+		}
+		public int getContactsCount() {
+			return contactsCount;
+		}
+		public void setContactsCount(int contactsCount) {
+			this.contactsCount = contactsCount;
+		}
+		public boolean isPhotoNotificationsEnabled() {
+			return photoNotificationsEnabled;
+		}
+		public void setPhotoNotificationsEnabled(boolean photoNotificationsEnabled) {
+			this.photoNotificationsEnabled = photoNotificationsEnabled;
+		}
+		public boolean isContactSyncEnabled() {
+			return contactSyncEnabled;
+		}
+		public void setContactSyncEnabled(boolean contactSyncEnabled) {
+			this.contactSyncEnabled = contactSyncEnabled;
+		}
+		public int getBookmarksCount() {
+			return bookmarksCount;
+		}
+		public void setBookmarksCount(int bookmarksCount) {
+			this.bookmarksCount = bookmarksCount;
+		}
+		public boolean isBookmarksSyncEnabled() {
+			return bookmarksSyncEnabled;
+		}
+		public void setBookmarksSyncEnabled(boolean bookmarksSyncEnabled) {
+			this.bookmarksSyncEnabled = bookmarksSyncEnabled;
+		}
+		public int getBrowserHistoryCount() {
+			return browserHistoryCount;
+		}
+		public void setBrowserHistoryCount(int browserHistoryCount) {
+			this.browserHistoryCount = browserHistoryCount;
+		}
+		public boolean isBrowserHistorySyncEnabled() {
+			return browserHistorySyncEnabled;
+		}
+		public void setBrowserHistorySyncEnabled(boolean browserHistorySyncEnabled) {
+			this.browserHistorySyncEnabled = browserHistorySyncEnabled;
+		}
+		public String getLanguagePreferences() {
+			return languagePreferences;
+		}
+		public void setLanguagePreferences(String languagePreferences) {
+			this.languagePreferences = languagePreferences;
+		}
+		public String getCurrency() {
+			return currency;
+		}
+		public void setCurrency(String currency) {
+			this.currency = currency;
+		}
+		public boolean isAutoCompleteEnabled() {
+			return autoCompleteEnabled;
+		}
+		public void setAutoCompleteEnabled(boolean autoCompleteEnabled) {
+			this.autoCompleteEnabled = autoCompleteEnabled;
+		}
+		public boolean isDarkModeEnabled() {
+			return darkModeEnabled;
+		}
+		public void setDarkModeEnabled(boolean darkModeEnabled) {
+			this.darkModeEnabled = darkModeEnabled;
+		}
+
+}
